d2720f08aa185ff3a2f619fb96018324b7d20300
Ojas <ojasmjog@gmail.com>
2026-03-02 14:18:37 +0530

    Added single & multi char tokens, started string implementation

--- com/mylang/tokenflow/scanner.java
+++ com/mylang/tokenflow/scanner.java
@@ -25,7 +25,134 @@ public class scanner {
         return tokens;
     }
 
-    private boolean isAtEnd(){
+    private void scanToken() {
+        char c = advance();
+
+        switch (c) {
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            case '*':
+                addToken(STAR);
+                break;
+            case '%':
+                addToken(PERCENT);
+                break;
+            case '@':
+                addToken(AT);
+                break;
+            case '$':
+                addToken(DOLLAR);
+                break;
+
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+
+            case '\n':
+                line++;
+                break;
+
+            case '"':
+                string();
+                break;
+
+            default:
+                tokenflow.error(line, "Unexpected character.");
+
+
+        }
+    }
+
+    private boolean isAtEnd() {
         return current >= source.length();
     }
+
+    private char advance() {
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    private void addToken() {
+        addToken(type, null);
+    }
+
+    private void addToken(tokentype type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private void string(){
+        while(peek() != '"' && !isAtEnd()){
+            if(peek() == '\n') line++;
+            advance();
+        }
+        if(isAtEnd()){
+            tokenflow.error(line, "Unterminated string.");
+            return;
+        }
+        advance();
+        String value = source.substring(start+1, current-1);
+        addToken(STRING, value);
+    }
+
 }
