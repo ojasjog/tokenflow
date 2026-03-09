15fd8dddfd5b8d665662c8b22c01b533e678725d
Ojas <ojasmjog@gmail.com>
2026-03-09 22:22:38 +0530

    Added temporary AstPrinter, defined Expr.java, explored visitor design pattern

--- com/mylang/tool/GenerateAst.java
+++ com/mylang/tool/GenerateAst.java
@@ -32,22 +32,40 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+
+        defineVisitor(writer, baseName, types);
+
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
-
+        writer.println();
+        writer.println(" abstract <R> R accept(Visitor<R> visitor);");
         writer.println("}");
         writer.close();
 
     }
+
+    private static void defineVisitor(
+            PrintWriter writer, String baseName, List<String> types) {
+        writer.println(" interface Visitor<R> {");
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println(" R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+        writer.println(" }");
+    }
+
 
     public static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
         writer.println(" static class " + className + " extends " +
                 baseName + " {");
         // Constructor.
         writer.println(" " + className + "(" + fieldList + ") {");
+
+
         // Store parameters in fields.
         String[] fields = fieldList.split(", ");
         for (String field : fields) {
@@ -62,5 +80,14 @@ public class GenerateAst {
         }
 
         writer.println("    }");
+
+        writer.println();
+        writer.println(" @Override");
+        writer.println(" <R> R accept(Visitor<R> visitor) {");
+        writer.println(" return visitor.visit" +
+                className + baseName + "(this);");
+        writer.println(" }");
     }
+
+
 }
